5b397c4591d63b865b8fbf7de000c2c44f7df158
rowanedwards <rowancoope@gmail.com>
2021-06-23 11:31:27 +1000

    Video changed to vimeo

--- wp-content/themes/cut-landing/resources/views/front-page.blade.php
+++ wp-content/themes/cut-landing/resources/views/front-page.blade.php
@@ -5,19 +5,22 @@
     {{-- @include('partials.slider') --}}
 
   <div class="videoWrapper">
-    <div class="plyr__video-embed" id="player">
+   <div class="plyr__video-embed" id="player">
       <iframe
-        src="https://www.youtube.com/watch?v=4gtJc53CXsM"
+        src="https://player.vimeo.com/video/566130862?loop=true&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media"
+        allowfullscreen
         allowtransparency
         allow="autoplay"
       ></iframe>
     </div>
+    {{-- <iframe class="home-video" id="player_1" src="https://player.vimeo.com/video/566130862?background=?api=1&player_id=player_1" width="100%" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> --}}
   </div>
 
 <script> 
   //['captions', 'quality', 'speed', 'loop']
   const player 	= new Plyr('#player', {
     autoplay: true,
+    controls: false,
   });
 
   // Expose player so it can be used from the console
