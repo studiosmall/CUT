919214979580010fcbd7cd5b92b1f8e6d1a2abd1
rowanedwards <rowancoope@gmail.com>
2021-06-23 11:41:10 +1000

    Cache for WP

--- wp-content/themes/cut-landing/config/view.php
+++ wp-content/themes/cut-landing/config/view.php
@@ -29,7 +29,8 @@ return [
     |
     */
 
-    'compiled' => wp_upload_dir()['basedir'].'/cache',
+    //'compiled' => wp_upload_dir()['basedir'].'/cache',
+    'compiled' => '/tmp/sage-cache',
 
 
     /*
